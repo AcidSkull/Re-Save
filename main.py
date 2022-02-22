b673d87c4a476e207ee07d7e196f1c6827ef4626
Patryk Jasik <zielonyfantom@interia.pl>
2022-02-22 20:07:05 +0100

    Auth test

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
-import os
+import os, random
 
 app = Flask(__name__)
 
+REDDIT_TOKEN = os.environ.get("REDDIT_TOKEN")
+CLIENT_ID = os.environ.get("CLIENT_ID")
+RANDOM_STRING = random.randint(16)
+URI = 'https://savescraperforreddit.herokuapp.com'
+
+url = f"https://www.reddit.com/api/v1/authorize?client_id={CLIENT_ID}&response_type=code&state={RANDOM_STRING}&redirect_uri={URI}&duration=temporary&scope=history"
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', auth=url)
 
 if __name__ == '__main__':
     port = os.environ.get('PORT', 5000)
