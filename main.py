418d20bfe2d9e0d71e3a0a81f3c5902c6ecb4a90
Patryk Jasik <zielonyfantom@interia.pl>
2022-02-23 20:42:47 +0100

    Testing getting data from Reddit API

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
 from flask import Flask, render_template, request
+from flask_caching import Cache
+from uuid import uuid4
 import os, random, requests
 
 app = Flask(__name__)
 
 CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 CLIENT_ID = os.environ.get("CLIENT_ID")
-RANDOM_STRING = str(random.randint(16, 255))
 URI = 'https://savescraperforreddit.herokuapp.com'
 
-url = f"https://www.reddit.com/api/v1/authorize?client_id={CLIENT_ID}&response_type=code&state={RANDOM_STRING}&redirect_uri={URI}&duration=temporary&scope=history"
+
+def get_access_token():
+    auth = requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
+    
+    code = request.args.get('code') 
+    data = {'grant_type' : 'authorization_code',
+            'code' : code,
+            'redirect_uri' : URI}
+
+    headers = {'User-agent' : 'SaveScraper 0.1 by u/AciidSkull'}
+
+    access_token = requests.post('https://ssl.reddit.com/api/v1/access_token', auth=auth, data=data, headers=headers)
+    token = access_token.json()
+
+    return token['access_token']
 
 @app.route('/')
 def index():
-    if request.args.get('code'):
-        access_token = requests.post('https://ssl.reddit.com/api/v1/access_token', 
-        auth=requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET),
-        data={"grant_type":'authorization_code', "code":f"{request.args.get('code')}", "redirect_uri": URI},
-        headers={'User-agent' : 'Save Scraper 0.0.1', 'Content-Type': 'application/x-www-form-urlencoded'})
-        print(access_token)
-        return render_template('index.html', auth=url)
-    return render_template('index.html', auth=url)
+    random_string = str(uuid4())
+    url = f"https://www.reddit.com/api/v1/authorize?client_id={CLIENT_ID}&response_type=code&state={random_string}&redirect_uri={URI}&duration=temporary&scope=history"
+    Obj = None
+    if (request.args.get('code')):
+        Token = get_access_token()
+        response = requests.get("https://oauth.reddit.com/api/v1/me", headers={"Authorization" : "bearer " + Token}).json()
+        Obj = response['name']
+
+    return render_template('index.html', auth_url=url, Obj=Obj)
 
 if __name__ == '__main__':
     port = os.environ.get('PORT', 5000)
