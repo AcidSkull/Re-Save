c0b979ce21a9ebe5b6c831b22086cd9f92f4ed1e
Patryk Jasik <zielonyfantom@interia.pl>
2022-02-23 17:06:26 +0100

    Testing getting access token 20

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, request
-from requests.auth import HTTPBasicAuth
 import os, random, requests
 
 app = Flask(__name__)
 
-REDDIT_TOKEN = os.environ.get("REDDIT_TOKEN")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 CLIENT_ID = os.environ.get("CLIENT_ID")
 RANDOM_STRING = str(random.randint(16, 255))
 URI = 'https://savescraperforreddit.herokuapp.com'
@@ -14,9 +13,9 @@ url = f"https://www.reddit.com/api/v1/authorize?client_id={CLIENT_ID}&response_t
 @app.route('/')
 def index():
     if request.args.get('code'):
-        access_token = requests.post('https://reddit.com/api/v1/access_token', 
-        auth=HTTPBasicAuth(CLIENT_ID, REDDIT_TOKEN),
-        data={"grant_type":'authorization_code', "code":f"{request.args.get('code')}", "redirect_uri":f"{URI}"},
+        access_token = requests.post('https://ssl.reddit.com/api/v1/access_token', 
+        auth=requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET),
+        data={"grant_type":'authorization_code', "code":f"{request.args.get('code')}", "redirect_uri": URI},
         headers={'User-agent' : 'Save Scraper 0.0.1', 'Content-Type': 'application/x-www-form-urlencoded'})
         print(access_token)
         return render_template('index.html', auth=url)
