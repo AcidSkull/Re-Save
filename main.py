21c987caaf6a8d87ef28cd5ddbd12699b8608dcb
Patryk Jasik <zielonyfantom@interia.pl>
2022-02-23 13:06:30 +0100

    Getting access token test 8

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ url = f"https://www.reddit.com/api/v1/authorize?client_id={CLIENT_ID}&response_t
 @app.route('/')
 def index():
     if request.args.get('code'):
-        access_token = requests.post('https://www.reddit.com/api/v1/access_token', 
+        access_token = requests.post('https://reddit.com/api/v1/access_token', 
         data={"grant_type":'authorization_code', "code":f"{request.args.get('code')}", "redirect_uri":f"{URI}"}),
-        header={'Authorization' : f'{HTTPBasicAuth(CLIENT_ID, REDDIT_TOKEN)}', 'User-agent' : 'Save Scraper 0.0.1'}
+        header={'User-agent' : 'Save Scraper 0.0.1', 'Authorization' : f'{HTTPBasicAuth(CLIENT_ID, REDDIT_TOKEN)}', 'Content-Type': 'application/x-www-form-urlencoded'}
         print(access_token)
         return 'access_token'
     return render_template('index.html', auth=url)
