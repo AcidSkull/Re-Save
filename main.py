42cc584204c35f254e8a2f31b13e043dcd3d674f
Patryk Jasik <zielonyfantom@interia.pl>
2022-02-27 16:51:41 +0100

    Added parsing Reddit API response to simpler array

--- main.py
+++ main.py
@@ -42,28 +42,38 @@ def get_saved_posts(Token):
     else:
         return None
 
+def parse_reddit_api_response(saved_posts):
+    parsed_response = []
+    for post in saved_posts['data']['children']:
+        score = post['data']['score']
+        subreddit_name = post['data']['subreddit_name_prefixed']
+        author = post['data']['author']
+        date = datetime.fromtimestamp(post['data']['created_utc'])
+        permalink = 'https://www.reddit.com' + post['data']['permalink']
+        title = post['data']['title']
+        selftext = post['data']['selftext']
+        img_url = post['data']['url']
 
-@app.template_filter('dateformat')
-def dateformat(value):
-    return datetime.fromtimestamp(value)
+        parsed_response.append([score, subreddit_name, author, date, permalink, title, selftext, img_url])
 
 @app.route('/')
 def index():
-    saved_posts = {}
+    saved_posts = None
 
-    random_string = str(uuid4())
-    url = f"https://www.reddit.com/api/v1/authorize?client_id={CLIENT_ID}&response_type=code&state={random_string}&redirect_uri={URI}&duration=temporary&scope=identity,read,history"
+    if not(session.get('user')):
+        random_string = str(uuid4())
+        url = f"https://www.reddit.com/api/v1/authorize?client_id={CLIENT_ID}&response_type=code&state={random_string}&redirect_uri={URI}&duration=temporary&scope=identity,read,history"
 
-    if (request.args.get('code')):
-        session['Token'] = get_access_token(request.args.get('code'))
+        if (request.args.get('code')):
+            session['Token'] = get_access_token(request.args.get('code'))
 
-        if session['Token'] != None:
-            user = get_user_info(session['Token'])
-            session['name'] = user['name']
-            saved_posts = get_saved_posts(session['Token'])
+            if session['Token'] != None:
+                user = get_user_info(session['Token'])
+                session['name'] = user['name']
+                saved_posts = get_saved_posts(session['Token'])
             
 
-    if (saved_posts == {}) and (session.get('user')):
+    if (saved_posts == None) and (session.get('user')):
         session.pop('user')
         
     return render_template('index.html', auth_url=url, saved_posts=saved_posts)
