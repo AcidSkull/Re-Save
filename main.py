24727c161c90fa74bbc6f367dd47f76a3aa5531c
Patryk Jasik <zielonyfantom@interia.pl>
2022-03-01 14:29:19 +0100

    Fixing refresh error 4

--- main.py
+++ main.py
@@ -12,6 +12,13 @@ URI = 'https://savescraperforreddit.herokuapp.com'
 USER_AGENT = 'SaveScraperForReddit/0.2.1 by u/AciidSkull'
 SCOPE = ['identity', 'read', 'history']
 
+reddit = praw.Reddit(
+        client_id = CLIENT_ID,
+        client_secret = CLIENT_SECRET,
+        redirect_uri = URI,
+        user_agent = USER_AGENT,
+    )
+
 def parse_reddit_api_response(saved_posts):
     parsed_response = []
 
@@ -34,36 +41,18 @@ def index():
     saved_posts = []
     url = ''
 
-    reddit = praw.Reddit(
-        client_id = CLIENT_ID,
-        client_secret = CLIENT_SECRET,
-        redirect_uri = URI,
-        user_agent = USER_AGENT,
-    )
-
-    if(request.args.get('code') and not session.get('Token')):
+    if(not request.args.get('code')):
         session['code'] = request.args.get('code')
-        session['Token'] = reddit.auth.authorize(session['code'])
-        session['name'] = str(reddit.user.me())
+
+        if not session.get('Token'):
+            session['Token'] = reddit.auth.authorize(session['code'])
+            session['name'] = str(reddit.user.me())
 
         response = {x.id:x for x in reddit.redditor(name=session['name']).saved(limit=None)}
         saved_posts = parse_reddit_api_response(response)
     else:
         random_string = str(uuid4)
         url = reddit.auth.url(SCOPE, random_string, 'permanent')
-
-    # if (request.args.get('code')):
-    #     if not session.get('Token'):
-    #         session['Token'] = reddit.auth.authorize(request.args.get('code'))
-
-    #     name = str(reddit.user.me())
-    #     session['name'] = name
-    #     reddit_saved_posts = {x.id:x for x in reddit.redditor(name=name).saved(limit=None)}
-    #     saved_posts = parse_reddit_api_response(reddit_saved_posts)
-    # else:
-    #     random_string = str(uuid4())
-    #     url = reddit.auth.url(SCOPE, random_string, 'permanent')
-            
         
     return render_template('index.html', auth_url=url, saved_posts=saved_posts)
 
