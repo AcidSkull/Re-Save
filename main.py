da0022c0aae907a967d07079049ee6894e2e6e4c
Patryk Jasik <zielonyfantom@interia.pl>
2022-03-01 22:15:13 +0100

    Fixed typo in index.html

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def parse_reddit_api_response(saved_posts):
             datetime.fromtimestamp(post.created_utc),
             'https://reddit.com' + str(post.permalink),
             str(post.title),
-            str(post.selftext_html),
+            post.selftext_html,
             str(post.url),
         ])
 
