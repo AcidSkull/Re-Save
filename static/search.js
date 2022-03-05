8bf506c5c2eb1fcfc57be2a1c189688095558c79
Patryk Jasik <zielonyfantom@interia.pl>
2022-03-05 18:26:23 +0100

    Added search function

---
+++ static/search.js
@@ -0,0 +1,14 @@
+Posts = document.getElementsByClassName('posts')
+Titles = document.getElementsByClassName('title');
+
+function Search_Site(){
+    let pattern = document.getElementById('searchbox');
+
+    for(let i = 0; i < Titles.length(); ++i){
+        if(Titles.textContent == pattern || pattern == ""){
+            Posts[i].style.display = 'none';
+        } else {
+            Posts[i].style.display = '';
+        }
+    }
+}
