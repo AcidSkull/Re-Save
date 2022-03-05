d7239e7bed7839a77af4fa2f0159d8bbdd6a6b11
Patryk Jasik <zielonyfantom@interia.pl>
2022-03-05 20:12:33 +0100

    Fixed typo

--- static/search.js
+++ static/search.js
@@ -5,10 +5,10 @@ Searchbox = document.getElementById('searchbox');
 
 Searchbox.addEventListener('input', () => {
     let pattern = document.getElementById('searchbox').value;
-    let regex = new RegExp(pattern)
+    let regex = new RegExp(pattern, "ig")
 
     for(let i = 0; i < Titles.length; ++i){
-        if(regex.test(Titles.textContent) || pattern == ''){
+        if(regex.test(Titles[i].textContent) || pattern == ''){
             Posts[i].style.display = '';
         } else {
             Posts[i].style.display = 'none';
