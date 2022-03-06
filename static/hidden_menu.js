93a47239a3bb760c02552bbdc5ae5bafc76b62c0
Patryk Jasik <zielonyfantom@interia.pl>
2022-03-06 22:14:32 +0100

    Added subreddits buttons to accordion menu

--- static/hidden_menu.js
+++ static/hidden_menu.js
@@ -1,9 +1,30 @@
-button = document.getElementsByClassName('settings')
-menu = document.getElementById('hidden_menu');
+let button = document.getElementsByClassName('settings');
+let menu = document.getElementById('hidden_menu');
 
+// Opening/closing accordion menu
 button[0].addEventListener('click', function(){
     if(menu.style.maxHeight == menu.scrollHeight + 'px')
         menu.style.maxHeight = 0; 
     else 
-        menu.style.maxHeight = menu.scrollHeight+'px';
+        menu.style.maxHeight = menu.scrollHeight + 'px';
 });
+
+// Function to filter unique values from array
+const Unique = (value, index, self) => {
+    return self.indexOf(value) === index;
+}
+
+// Creating array with names of subreddits
+let subreddit = document.getElementsByClassName('subreddit');
+let subreddits = new Array;
+for(let i = 0; i < subreddit.length; i++){
+    subreddits[i] = subreddit[i].innerHTML;
+}
+
+// Creating array with unique subreddits names
+let subreddits_unique = subreddits.filter(Unique);
+
+// Creating buttons in hidden menu
+for(let i = 0; i < subreddits_unique.length; i++){
+    menu.innerHTML += `<div class="category">${subreddits_unique[i]}</div>`;
+}
--- static/search.js
+++ static/search.js
@@ -3,9 +3,11 @@ Titles = document.getElementsByClassName('title');
 Searchbox = document.getElementById('searchbox');
 
 let search = function (){
+    // Seting regular expression equal to text in search bar
     let pattern = document.getElementById('searchbox').value;
     let regex = new RegExp(pattern, "ig")
 
+    // Looping through all posts and checking if title is equal to regex pattern or if searchbox is empty, then if true make post visible
     for(let i = 0; i < Titles.length; ++i){
         if(regex.test(Titles[i].textContent) || pattern == ''){
             Posts[i].style.display = '';
