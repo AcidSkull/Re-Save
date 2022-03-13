d51db1c97b66d24ea99cda987283c3649d46f866
Patryk Jasik <zielonyfantom@interia.pl>
2022-03-13 21:48:16 +0100

    Added hiding posts by subreddits

--- static/hidden_menu.js
+++ static/hidden_menu.js
@@ -28,3 +28,42 @@ let subreddits_unique = subreddits.filter(Unique);
 for(let i = 0; i < subreddits_unique.length; i++){
     menu.innerHTML += `<div class="category">${subreddits_unique[i]}</div>`;
 }
+
+// Hide unwanted posts
+let flags = [];
+function hide(){
+    let posts = document.getElementsByClassName('post');
+    let sub = document.getElementsByClassName('subreddit');
+
+    for(let i = 0; i < posts.length; i++){
+        if(flags.length == 0) {
+            posts[i].style.display = '';
+            continue;
+        }
+
+        for(let j = 0; j < flags.length; j++){
+            if(sub[i].textContent == flags[j]){
+               posts[i].style.display = ''; 
+               break;
+            } else {
+                posts[i].style.display = 'none'; 
+            }
+        }
+    }
+}
+
+// Adding click event to all subreddits buttons
+let category = document.getElementsByClassName('category');
+for(let i = 0; i < category.length; i++){
+    category[i].addEventListener('click', function(){
+        this.classList.toggle('chosen');
+        
+        if(this.classList.contains('chosen') == true){
+            flags.push(this.textContent);
+        } else {
+            let to_delete = flags.indexOf(this.textContent);
+            flags.splice(to_delete, 1);
+        }
+        hide();
+    });
+}
