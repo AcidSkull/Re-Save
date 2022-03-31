b2f53fafb1554a5e380042b0aa10ece5cc0fcb7d
Patryk Jasik <zielonyfantom@interia.pl>
2022-03-31 21:04:08 +0200

    Delete debug=True

--- main.py
+++ main.py
@@ -85,4 +85,4 @@ def logout():
 # Getting env variable PORT and running flask app
 if __name__ == '__main__':
     port = os.environ.get('PORT', 5000)
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
